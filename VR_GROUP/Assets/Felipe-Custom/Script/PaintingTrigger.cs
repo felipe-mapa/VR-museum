ec63ac3ac5d16cf55583599e504e6c9924c08866
Felipe Pavanela <felipe-pavanela@hotmail.com>
2019-06-06 23:35:15 +1200

    Felipe custom folder & scene - please read description
    Hey guys I just added a new custom folder and a new scene called Felipe-custom. I created a very ugly room with a plane on the wall to simulate a painting. 
    I created a script called PaintingTrigger that is located in my folder.
    It is a simple scrip that does what we need for now. When we focus on the "painting" the name of the painting shows in the top and when we look away, it hides. Also, if we look at the painting and click, it plays a song (what I think can be some infos of the painting) and when we look away it stops. Hope it can be of some use for you guys.

---
+++ VR_GROUP/Assets/Felipe-Custom/Script/PaintingTrigger.cs
@@ -0,0 +1,36 @@
+namespace GoogleVR.HelloVR
+{
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using TMPro;
+
+    [RequireComponent(typeof(Collider))]
+    public class PaintingTrigger : MonoBehaviour
+    {
+        // VARIABLES FOR PAINTING 
+        private TextMeshPro paintingName;
+        public AudioSource audioClass;
+
+
+        void Start()
+        {
+            paintingName = this.GetComponentInChildren<TextMeshPro>();
+            IsFocused(false);
+        }
+
+        public void IsFocused(bool focused)
+        {
+            paintingName.enabled = focused;
+
+            if (!focused){
+                audioClass.Stop();
+            }
+            return;
+        }
+
+        public void PlayAudio(){
+            audioClass.Play(0);
+            Debug.Log("started");
+        }
+    }
+}
